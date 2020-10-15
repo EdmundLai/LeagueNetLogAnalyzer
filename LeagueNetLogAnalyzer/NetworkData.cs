110bacc9d3aa187370dd9a76a6fb5046035519d0
Edmund Lai <edmund.lai.07@gmail.com>
2020-10-14 17:48:37 -0700

    Initial commit

---
+++ LeagueNetLogAnalyzer/NetworkData.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeagueNetLogAnalyzer
+{
+    class NetworkData
+    {
+        /// <summary>
+        /// time = time since start of game in ms.
+        /// </summary>
+        public int Time { get; set; }
+
+        /// <summary>
+        /// address = address of IP address
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// incoming = total size of messages set to me in bytes. Spikes can indicate lag.
+        /// </summary>
+        public int Incoming { get; set; }
+
+        /// <summary>
+        /// outgoing = total size of messages set from me in bytes. Spikes can indicate lag.
+        /// </summary>
+        public int Outgoing { get; set; }
+
+        /// <summary>
+        /// app_ctos = client to server bytes delta excluding overheads (app data only)
+        /// </summary>
+        /// 
+        public int AppCtos { get; set; }
+
+        /// <summary>
+        /// app_stoc = server to client bytes delta excluding overheads (app data only)
+        /// </summary>
+        public int AppStoc { get; set; }
+
+        /// <summary>
+        /// loss = number of packets lost. Can indicate lags.  Some node on the network is dropping packets or taking to long and we have to resend them.
+        /// </summary>
+        public int Loss { get; set; }
+
+        /// <summary>
+        /// sent = number of packets sent. Spikes can indicate lag.
+        /// </summary>
+        public int Sent { get; set; }
+
+        /// <summary>
+        /// ping = how long it takes for a packet to get back and forth. Spikes can indicate lag.
+        /// </summary>
+        public int Ping { get; set; }
+
+        /// <summary>
+        /// variance = how much the ping is changing from avg ping. Spikes can indicate lag.
+        /// </summary>
+        public int Variance { get; set; }
+
+        /// <summary>
+        /// reliable delayed = This occurs when we drop reliable packets from sending this frame because we are sending to much data. Unreliable are more likely to be dropped than reliable.
+        /// </summary>
+        public int ReliableDelayed { get; set; }
+
+        /// <summary>
+        /// unreliable delayed = This occurs when we drop unreliable packets from sending this frame because we are sending too much data.
+        /// </summary>
+        public int UnreliableDelayed { get; set; }
+
+        /// <summary>
+        /// app update delayed = This occurs when the app is taking to long to run code caused by network packets (ie CPU) and packets are delayed until the next frame
+        /// </summary>
+        public int AppUpdateDelayed { get; set; }
+
+        /// <summary>
+        /// Time spent in critical section (frame) = This is the time the network thread is blocking the main thread from doing anything.
+        /// </summary>
+        public int CritSectionTime { get; set; }
+
+        /// <summary>
+        /// Latency in Window = The average Latency in ms. of Reliable packets during this time slice
+        /// </summary>
+        public int LatencyInWindow { get; set; }
+
+        /// <summary>
+        /// Packet Loss percentage in Window = The packet loss percentage of Reliable packets during this time slice
+        /// </summary>
+        public double PacketLossPercentageInWindow { get; set; }
+
+        /// <summary>
+        /// Jitter in Window = The average Jitter in ms. of Reliable packets during this time slice
+        /// </summary>
+        public int JitterInWindow { get; set; }
+
+        /// <summary>
+        /// Overall Latency Min = The lowest Latency seen in ms. of Reliable packets up to this point in time
+        /// </summary>
+        public int OverallLatencyMin { get; set; }
+
+        /// <summary>
+        /// Overall Latency Max = The largest Latency seen in ms. of Reliable packets up to this point in time
+        /// </summary>
+        public int OverallLatencyMax { get; set; }
+
+        /// <summary>
+        /// Latency Min in Window = The lowest Latency seen in ms. of Reliable packets during this time slice
+        /// </summary>
+        public int LatencyMinInWindow { get; set; }
+
+        /// <summary>
+        /// Latency Max in Window = The largest Latency seen in ms. of Reliable packets during this time slice
+        /// </summary>
+        public int LatencyMaxInWindow { get; set; }
+
+        /// <summary>
+        /// Latency packet samples = number of packets used in determining average latency from start of game
+        /// </summary>
+        public int LatencyPacketSamples { get; set; }
+
+        /// <summary>
+        /// Jitter packet samples = number of packets used in determining average jitter from start of game
+        /// </summary>
+        public int JitterPacketSamples { get; set; }
+
+        /// <summary>
+        /// reconnect = flag to indicate one or more reconnects occurred
+        /// </summary>
+        public int Reconnect { get; set; }
+
+
+    }
+}
---
+++ LeagueNetLogAnalyzer/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace LeagueNetLogAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string testFolder = @"C:\Riot Games\League of Legends\Logs\GameLogs\2020-10-12T23-00-17";
+            //AnalyzeNetworkData(GetNetLogContents(testFolder));
+            AnalyzeLatestNetLogData();
+            
+        }
+
+        static void AnalyzeLatestNetLogData()
+        {
+            string[] netLogFolders = System.IO.Directory.GetDirectories(@"C:\Riot Games\League of Legends\Logs\GameLogs");
+            DateTime latestDate = new DateTime(0);
+            string latestFolder = "";
+            foreach (string folder in netLogFolders)
+            {
+                //Console.WriteLine(folder);
+                //string[] folderFiles = Directory.GetFiles(folder);
+
+                string lastDirectory = new DirectoryInfo(folder).Name;
+
+                //Console.WriteLine(lastDirectory);
+
+
+                DateTime gameDate = DateTime.ParseExact(lastDirectory, "yyyy-MM-ddTHH-mm-ss", CultureInfo.InvariantCulture);
+                //Console.WriteLine(gameDate.ToString());
+
+                if(gameDate > latestDate)
+                {
+                    latestDate = gameDate;
+                    latestFolder = folder;
+                }
+
+                //Console.WriteLine(gameDate.ToString("yyyy-MM-dd"));
+            }
+
+            if (!String.IsNullOrEmpty(latestFolder))
+            {
+                Console.WriteLine($"Ping data from latest game:");
+                AnalyzeNetworkData(GetNetLogContents(latestFolder));
+            } else
+            {
+                Console.WriteLine("No network data available to analyze!");
+            }
+            
+        }
+
+
+        static List<NetworkData> GetNetLogContents(string folder)
+        {
+            string lastDirectory = new DirectoryInfo(folder).Name;
+
+            //Console.WriteLine(lastDirectory);
+
+            DateTime gameDate = DateTime.ParseExact(lastDirectory, "yyyy-MM-ddTHH-mm-ss", CultureInfo.InvariantCulture);
+            //Console.WriteLine(gameDate.ToString());
+            DateTime gameDateTmr = gameDate.AddDays(1);
+
+            // possible prefixes to non-data portions of log
+            string[] prefixes = { gameDate.ToString("yyyy-MM-dd"), gameDateTmr.ToString("yyyy-MM-dd") };
+
+            // path to netlog file
+            string netlogPath = Path.Combine(folder, $"{lastDirectory}_netlog.txt");
+
+            //Console.WriteLine(netlogPath);
+
+            string[] netlogContents = System.IO.File.ReadAllLines(netlogPath);
+
+            List<string> netlogData = FilterNetLogContents(netlogContents, prefixes);
+
+            List<NetworkData> networkDatas = new List<NetworkData>();
+
+            // it works! :)
+            foreach(var line in netlogData)
+            {
+                //Console.WriteLine(line);
+                networkDatas.Add(ConvertLineToNetworkData(line));
+            }
+
+            //Console.WriteLine($"number of network data received: {networkDatas.Count}");
+
+            return networkDatas;
+        }
+
+        static void AnalyzeNetworkData(List<NetworkData> networkDatas)
+        {
+            foreach(var data in networkDatas)
+            {
+                double numGameMin = Math.Round(((double) data.Time / 60000), 2);
+                Console.WriteLine($"Ping at {numGameMin} minutes: {data.Ping} ms");
+            }
+        }
+
+        static NetworkData ConvertLineToNetworkData(string line)
+        {
+            //Console.WriteLine(line);
+            string[] lineData = line.Split(",");
+
+            double packetLossPercent;
+            if(!double.TryParse(lineData[15], out packetLossPercent))
+            {
+                //Console.WriteLine($"{lineData[15]} was not a double.");
+                packetLossPercent = 0;
+            }
+
+            var networkData = new NetworkData
+            {
+                Time = Int32.Parse(lineData[0]),
+                Address = lineData[1],
+                Incoming = Int32.Parse(lineData[2]),
+                Outgoing = Int32.Parse(lineData[3]),
+                AppCtos = Int32.Parse(lineData[4]),
+                AppStoc = Int32.Parse(lineData[5]),
+                Loss = Int32.Parse(lineData[6]),
+                Sent = Int32.Parse(lineData[7]),
+                Ping = Int32.Parse(lineData[8]),
+                Variance = Int32.Parse(lineData[9]),
+                ReliableDelayed = Int32.Parse(lineData[10]),
+                UnreliableDelayed = Int32.Parse(lineData[11]),
+                AppUpdateDelayed = Int32.Parse(lineData[12]),
+                CritSectionTime = Int32.Parse(lineData[13]),
+                LatencyInWindow = Int32.Parse(lineData[14]),
+                PacketLossPercentageInWindow = packetLossPercent,
+                JitterInWindow = Int32.Parse(lineData[16]),
+                OverallLatencyMin = Int32.Parse(lineData[17]),
+                OverallLatencyMax = Int32.Parse(lineData[18]),
+                LatencyMinInWindow = Int32.Parse(lineData[19]),
+                LatencyMaxInWindow = Int32.Parse(lineData[20]),
+                LatencyPacketSamples = Int32.Parse(lineData[21]),
+                JitterPacketSamples = Int32.Parse(lineData[22]),
+                Reconnect = Int32.Parse(lineData[23]),
+            };
+
+            return networkData;
+        }
+
+        // filters netlog of any extraneous text data meant for humans
+        static List<string> FilterNetLogContents(string[] netlogContents, string[] prefixes)
+        {
+            return netlogContents.Where(line => !prefixes.Any(prefix => line.StartsWith(prefix))).ToList();
+        }
+    }
+}
